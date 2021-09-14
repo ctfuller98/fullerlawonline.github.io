c47cd913557522f9650c62c885bf532da1f17fd5
ctfuller98 <ful17005@byui.edu>
2021-09-14 10:31:38 -0600

    changes

---
+++ scripts.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+  });
+  function open_nav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+  }
